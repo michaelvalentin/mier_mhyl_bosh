7bee850d375b3076389c86999638cf6df009554e
root <root@boscubuntu.tietgenkollegiet.dk>
2012-09-30 15:46:18 +0200

    Added a demo for testing background processes.

---
+++ waithello.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+  sleep(5);
+  puts("Five seconds");
+  sleep(10);
+  puts("Fifteen seconds");
+  sleep(15);
+  puts("Done after 30 seconds");
+}
